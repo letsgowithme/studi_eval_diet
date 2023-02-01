704dbc5f7477c36c875901c87eded5157897c89b
iryna <musicira@gmail.com>
2023-02-01 16:29:28 +0100

    Ingredient.php correction

---
+++ src/Entity/Ingredient.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\IngredientRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: IngredientRepository::class)]
+class Ingredient
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    #[Assert\NotBlank()]
+    #[Assert\Length(min: 2, max: 50)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    #[Assert\NotBlank()]
+    private ?bool $allergen = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function isAllergen(): ?bool
+    {
+        return $this->allergen;
+    }
+
+    public function setAllergen(bool $allergen): self
+    {
+        $this->allergen = $allergen;
+
+        return $this;
+    }
+}
